b9d86023c64417a71daadc4f636373719154e1e9
Manoj Mannam <manojmannamapj9@gmail.com>
2025-08-28 10:47:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio0/', // Update this to match your repository name
-  server: {
-    port: 3000,
-    open: true
-  }
 })
+
